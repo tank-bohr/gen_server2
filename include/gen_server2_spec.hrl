6a80d7f53f5f7b84451c39d8652dc38b7a331660
Alexey Nikitin <tank@bohr.su>
2019-11-02 13:20:34 +0300

    Revert "Move callback out to hrl-file"
    This reverts commit 33dbc3221b5fe302699ee1965a9499f8d24e827f.

--- include/gen_server2_spec.hrl
+++
@@ -1,9 +0,0 @@
--ifndef(GEN_SERVER2_SPEC_HRL).
--define(GEN_SERVER2_SPEC_HRL, true).
-
--spec proc(Request, FromOrReason, State) -> #ok{} | #reply{} | #stop{} when
-    Request      :: init | terminate | term(),
-    FromOrReason :: pid() | {pid(), reference()} | term(),
-    State        :: term().
-
--endif.
